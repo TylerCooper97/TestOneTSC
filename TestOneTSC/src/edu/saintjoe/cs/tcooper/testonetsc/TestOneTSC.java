c851a648db818ea680bd28c1c976917bb882df00
tcooper <tcooper@localhost>
2016-02-26 16:37:04 -0500

    Added code so that program says my name 6 times

--- TestOneTSC/src/edu/saintjoe/cs/tcooper/testonetsc/TestOneTSC.java
+++ TestOneTSC/src/edu/saintjoe/cs/tcooper/testonetsc/TestOneTSC.java
@@ -3,7 +3,12 @@ package edu.saintjoe.cs.tcooper.testonetsc;
 public class TestOneTSC {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int i = 6;
+		while (i > 0) {
+			System.out.println("Tyler Cooper");
+			i = i - 1;
+		}
+		
 
 	}
 
